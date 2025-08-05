4c4ed2d665ae6c8bbb21a277b11dcd3a258d99e4
Gabriel Developer <gabrielnovaes@yahoo.com.br>
2025-08-05 06:01:50 -0300

    feat: added constants

---
+++ lib/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const cardBackgroundColor = Color(0xFF21222D);
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor = Color(0xFFFFFFFF);
+const backgroundColor = Color(0xFF15131c);
+const selectionColor = Color(0xFF88b2ac);
+const defautlPadding = 20.0;
